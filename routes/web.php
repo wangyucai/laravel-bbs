184955e1edf51f00189b3bb0dc9ecc18d2a9d70f
vagrant <vagrant@homestead>
2017-12-25 09:35:30 +0000

    SEO 友好的URL

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,8 @@ Auth::routes();
 /*用户个人中心*/
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 /*话题*/
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 /*分类*/
 Route::resource('categories', 'CategoriesController',['only' => ['show']]);
 /*创建话题--编辑器上传图片*/
