2a6fbccbddb1ed6bf2957025eaeaa78d74167011
vagrant <vagrant@homestead>
2017-12-25 06:24:42 +0000

    新建话题

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -4,3 +4,10 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+// 创建或编辑话题时用来填充话题的摘录，摘录由文章内容中自动生成，生成的时机是在话题数据存入数据库之前
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return str_limit($excerpt, $length);
+}
