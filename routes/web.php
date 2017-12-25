14d439c6e31434d3635dca6c7948a5ea937d5435
vagrant <vagrant@homestead>
2017-12-25 07:09:27 +0000

    在编辑器上传话题图片

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 /*话题*/
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 /*分类*/
-Route::resource('categories','CategoriesController',['only' => ['show']]);
+Route::resource('categories', 'CategoriesController',['only' => ['show']]);
+/*创建话题--编辑器上传图片*/
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
